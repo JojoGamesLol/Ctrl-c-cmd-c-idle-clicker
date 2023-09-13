c7ebc2432b458d2e798df2f23d8455b3b6869220
JojoGamesLol <143434175+JojoGamesLol@users.noreply.github.com>
2023-09-13 15:18:01 -0300

    Update info.js

--- info.js
+++ info.js
@@ -1,15 +1,16 @@
 // Account Info
 // // Account ID: FN04CbmTSkR8l2ahPJlTmZ1cQsC3
 // // Account Email: j9903171@gmail.com
-// // Levels Uploaded:
+// // Levels Uploaded: 1
 
 // Levels
 // // Group: Official Levels
 // // No Levels yet 😞
 // // Group: Non-Official Levels
 // // // Level: Ukraine Idler
-// // // Level Changelog:
-// // // v1.0.0
-// // // // Initial released
+// // // // Level Changelog:
+// // // // v1.0.0
+// // // // // Initial released
+// // // // Level ID: 61a62601-2a4d-4052-b734-f367e4d09e50
 // // Group: Level Errors or Not working
 // // No Levels yet 😞
