ead65a54e2c847918b87a2a0187a81f79ea79a44
JojoGamesLol <143434175+JojoGamesLol@users.noreply.github.com>
2023-09-13 15:13:00 -0300

    Update info.js

--- info.js
+++ info.js
@@ -2,3 +2,11 @@
 // // Account ID: FN04CbmTSkR8l2ahPJlTmZ1cQsC3
 // // Account Email: j9903171@gmail.com
 // // Levels Uploaded:
+
+// Levels
+// // Group: Official Levels
+// // No Levels yet 😞
+// // Group: Non-Official Levels
+// // // Level: Ukraine Idler
+// // Group: Level Errors or Not working
+// // No Levels yet 😞
